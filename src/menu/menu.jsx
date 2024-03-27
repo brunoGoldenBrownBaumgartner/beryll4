f032e1ed4a898f8aeefee67bc7cd62d7261a4694
brunoGoldenBrownBaumgartner <164511778+brunoGoldenBrownBaumgartner@users.noreply.github.com>
2024-03-27 05:11:11 +0100

    erster deploy

---
+++ src/menu/menu.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom"; // Korrekter Import von Link
+import './Menu.css';
+
+const SwipeMenu = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  return (
+    <div className="swipe-menu-container">
+      <div className={`menu ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><Link to="/" onClick={closeMenu}>💎</Link></li>
+          <li><Link to="/moon" onClick={closeMenu}>🌕</Link></li>
+          <li><a href="tel:0794630666" onClick={closeMenu}>CONTACT</a></li>
+        </ul>
+      </div>
+      <div className="content">
+        <button className="frites-menu-button" onClick={toggleMenu}>
+          <div className={`burger ${menuOpen ? 'open' : ''}`}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+        </button>
+        <p>Liebe Beryll, ich bin froh dass es Dich gibt!</p>
+      </div>
+      {menuOpen && <div className="overlay" onClick={closeMenu}></div>}
+    </div>
+  );
+};
+
+export default SwipeMenu;
